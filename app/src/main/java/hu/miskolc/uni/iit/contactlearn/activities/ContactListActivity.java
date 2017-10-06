174a8dc7e6e4095a064c1b22ba342e1d799ff66c
torok14 <torokkrisz@gmail.com>
2017-10-06 15:34:22 +0200

    ContactListActivity + layout + string

---
+++ app/src/main/java/hu/miskolc/uni/iit/contactlearn/activities/ContactListActivity.java
@@ -0,0 +1,15 @@
+package hu.miskolc.uni.iit.contactlearn.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import hu.miskolc.uni.iit.contactlearn.R;
+
+public class ContactListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
